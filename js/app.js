396c92658242878c431156ba5047fb609f793e8c
AdrixSC <adriana.be@outlook.com>
2018-02-14 12:56:05 -0600

    empezando codigo

---
+++ js/app.js
@@ -0,0 +1,17 @@
+$(document).ready(load(){
+   alert("hola!");
+});
+
+//Traer elementos
+
+var $btnLarge = $("#btn-large");
+var $btnMedium = $("#btn-medium");
+var $btnSmall = $("#btn-small");
+var $btnTextColor = $("#btn-text-color");
+var $btnBackagroundColor = $("#btn-bkg-color");
+var $btnLeft = $("#btn-left");
+var $btnCenter = $("#btn-center");
+var $btnRigth = $("#btn-rigth");
+
+var $textArea = $("#exampleFormControlTextarea1");
+var $containerComment = $("comment-container");
