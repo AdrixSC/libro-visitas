5c5a0de2b6e8865e755a615924e5b7092a63f78d
AdrixSC <adriana.be@outlook.com>
2018-02-15 13:20:54 -0600

    funcion para cambiar tamaño de texto, lista

--- js/app.js
+++ js/app.js
@@ -1,6 +1,4 @@
-var largeText = function() {
-    document.getElementById("btn-large").style.fontSize = 80 + "pt";
-}
+
 
 var mediumText = function() {
     document.getElementById("btn-medium").style.fontSize = 40 + "pt";
@@ -33,24 +31,26 @@ var rigthText = function() {
 }
 
 
+
 /*
 var containerComment = document.getElementById("comment-container");
 
 var btnLarge = addEventListener("click", largeText);*/
 
-addEventListener("click", largeText);
+/*addEventListener("click", largeText);
 
 var btnLarge = function(event) {
     var largeText = document.getElementById("btn-large").style.fontSize = 80 + "pt";
-
-}
+}*/
 
 //Traer elementos del html
 var textArea = document.getElementById('textarea-comment');
 var commentUser = document.getElementById("comment-user");
+var btnLarge = document.getElementById("btn-large");
 
 //Agregar eventos a los elementos
 textArea.addEventListener("keyup", viewText);
+btnLarge.addEventListener("click", largeText);
 //console.log(textArea.value);
 
 //Funcion para que se pinte en tiempo real el comentario del textarea
@@ -60,6 +60,18 @@ function viewText () {
   //console.log(textUser);
   return commentUser;
 }
+
+
+
+//Funcion para hacer el texto grande
+function largeText () {
+    commentUser.style.fontSize = "80px";
+}
+
+
+/*function styleFontLarge () {
+  textUser.style.fontSize = 80 + "pt";
+}*/
 
 
 /*var divContainerComment = document.getElementById("comment-container");
