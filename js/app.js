155d8499caa30950be04fddf2967d59882e3e9f0
Adriana Becerril <Adriana@MacBook-Air-de-Arisbeth.local>
2018-02-15 18:15:03 -0600

    cambios en la mac, no aceptarlo

--- js/app.js
+++ js/app.js
@@ -1,17 +1,60 @@
-$(document).ready(load(){
-   alert("hola!");
-});
+/*var largeText = function() {
+    document.getElementById("btn-large").style.fontSize = 80 + "pt";
+}
 
-//Traer elementos
+var mediumText = function() {
+    document.getElementById("btn-medium").style.fontSize = 40 + "pt";
+}
 
-var $btnLarge = $("#btn-large");
-var $btnMedium = $("#btn-medium");
-var $btnSmall = $("#btn-small");
-var $btnTextColor = $("#btn-text-color");
-var $btnBackagroundColor = $("#btn-bkg-color");
-var $btnLeft = $("#btn-left");
-var $btnCenter = $("#btn-center");
-var $btnRigth = $("#btn-rigth");
+var smallText = function() {
+    document.getElementById("btn-small").style.fontSize = 10 + "pt";
+}
 
-var $textArea = $("#exampleFormControlTextarea1");
-var $containerComment = $("comment-container");
+var colorText = function() {
+    var colorUser = prompt("Escribe el color que quieres en tu mensaje");
+    document.getElementById("btn-text-color").style.color = colorUser;
+}
+
+var backagroundColor = function() {
+    var backgroundUser = prompt("Escribe el color que quieres de fondo")
+    document.getElementById("btn-bkg-color").style.color = backgroundUser;
+}
+
+var leftText = function() {
+    document.getElementById("btn-left").style.textAlign = "left";
+}
+
+var centerText = function() {
+    document.getElementById("btn-center").style.textAlign = "center";
+}
+
+var rigthText = function() {
+    document.getElementById("btn-rigth").style.tectAlign = "rigth";
+}*/
+
+
+/*
+var containerComment = document.getElementById("comment-container");
+
+var btnLarge = addEventListener("click", largeText);*/
+
+var btnLarge = function(event) {
+    var largeText = document.getElementById("btn-large").style.fontSize = 80 + "pt";
+    addEventListener("click", largeText);
+}
+
+
+var text = function() {
+    var textArea = document.getElementById('exampleFormControlTextarea1').value;
+    console.log(textArea)
+}
+
+text();
+
+
+//var prueba = textArea.value;
+/*var divContainerComment = document.getElementById("comment-container");
+var divCommentUser = document.getElementById("comment-user").appendChild(textArea);
+divContainerComment.appendChild(divCommentUser);
+var divSectionComment = document.getElementById("comment-section");
+divSectionComment.appendChild(divContainerComment);*/
