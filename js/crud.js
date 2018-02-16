615b2b5271d1ca4d6a2261562888d49f1dcffaea
Adriana Becerril <Adriana@MacBook-Air-de-Arisbeth.local>
2018-02-15 21:10:28 -0600

    cambiando nombre de archivo de data

---
+++ js/crud.js
@@ -0,0 +1,43 @@
+var comments = [
+    hola = {
+        comment: "hola",
+        style: {
+            color: "pink",
+            backgroundColor: "yellow",
+            fontSize: "20px",
+            textAlign: "center"
+        }
+    },
+    adios = {
+        comment: "adios",
+        style: {
+            color: "yellow",
+            backgroundColor: "blue",
+            fontSize: "10px",
+            textAlign: "left"
+        }
+    }
+];
+
+//traer boton que ejecutará la acción de enviar comentario, darle el evento
+var btnSendComment = document.getElementById("btnSend");
+btnSendComment.addEventListener("click", drawComments);
+
+//crear elementos
+var divNewComment = document.createElement("div");
+var newComment = document.createElement("p");
+
+//agregar elementos a su padre y a un elemento existente
+divNewComment.appendChild(newComment);
+var containerComments = document.getElementById("container-comments");
+containerComments.appendChild(divNewComment);
+
+//funcion para pintar comentarios
+function drawComments() {
+    var commentNewUsser = commentUser.innerHTML;
+    comments.unshift(commentNewUsser);
+    console.log(comments);
+    //console.log(commentNewUsser);
+    //containerComments.appendChild(commentNewUsser);
+    newComment.appendChild(commentNewUsser);
+}
