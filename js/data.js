59ca6a06300eca229330fb81266deb6764f731ba
Adriana Becerril <Adriana@MacBook-Air-de-Arisbeth.local>
2018-02-15 20:02:13 -0600

    creando archivo data

---
+++ js/data.js
@@ -0,0 +1,20 @@
+var comments = [
+    hola = {
+        comment: "hola",
+        style: {
+            color: "pink",
+            backgroundColor: "yellow",
+            fontSize: "20px",
+            textAlign: "center"
+        }
+    },
+    adios = {
+        comment: "hola",
+        style: {
+            color: "pink",
+            backgroundColor: "yellow",
+            fontSize: "20px",
+            textAlign: "center"
+        }
+    }
+];
